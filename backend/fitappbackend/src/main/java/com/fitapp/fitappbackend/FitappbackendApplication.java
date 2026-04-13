0565e7166ac1087c0cad4bed95fb81c330ef7f7b
Juozas Paskauskas <paskauskasjuozas@gmail.com>
2026-04-13 15:32:07 +0300

    initial backend setup

---
+++ backend/fitappbackend/src/main/java/com/fitapp/fitappbackend/FitappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitapp.fitappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitappbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitappbackendApplication.class, args);
+    }
+
+}
---
+++ backend/fitappbackend/src/test/java/com/fitapp/fitappbackend/FitappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitapp.fitappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitappbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
